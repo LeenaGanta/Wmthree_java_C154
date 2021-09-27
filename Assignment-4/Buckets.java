8b1344818df10596b577152b4b25cd2428b62b3f
LeenaGanta <70707653+LeenaGanta@users.noreply.github.com>
2021-09-27 09:15:02 +0530

    Add files via upload

---
+++ Assignment-4/Buckets.java
@@ -0,0 +1,27 @@
+package sorts;
+
+import java.util.Scanner;
+
+public class Buckets 
+{
+	public static void main(String[] args)
+	   {
+		  Scanner sc=new Scanner(System.in);
+		  System.out.println("Enter no. of elements:");
+		  int n=sc.nextInt();
+		  int []arr=new int[n];
+		  for(int i=0;i<n;i++)
+		  {
+			  System.out.println("Enter number:");
+			  arr[i]=sc.nextInt();
+		  }
+		 System.out.println("Enter num of buckets");
+		 int nBuckets=sc.nextInt();
+		 
+		 BucketSort buckSort=new BucketSort();
+		 buckSort.bucketSort(arr, nBuckets);
+		 System.out.println("Elements after bucketSort are:");
+		 buckSort.display(arr, n);
+	   }
+
+}
